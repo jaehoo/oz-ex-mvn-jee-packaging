c588b815d0d94984d906393caa6bd545a10bf4ed
Alberto <jaehoo@gmail.com>
2012-06-04 13:20:05 -0500

    Test class to generate jar file

---
+++ jee-package/ejb-jar/src/main/java/TestClass.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 4/06/12
+ * Time: 01:16 PM
+ * Author: Lic. José Alberto Sánchez González
+ * Twitter: @jaehoox
+ * mail: <a href="mailto:jaehoo@gmail.com>jaehoo@gmail.com</a>
+ */
+public class TestClass {
+
+
+    public static void main(String[] args) {
+        System.out.println("EJB Test Class!!");
+    }
+}
